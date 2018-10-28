0292302b4be874eb0c99da08abcdf6838193e493
chenyuhan1 <1944614333@qq.com>
2018-10-28 23:38:32 +0800

    实现å实现基本框架，并实现首页新闻显示功能

---
+++ cloudfunctions/getData/index.js
@@ -0,0 +1,10 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    return await db.collection('news').skip((event.count-1)*5).limit(5).get()
+}
---
+++ cloudfunctions/getTopping/index.js
@@ -0,0 +1,9 @@
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    return await db.collection('topping').get()
+}
